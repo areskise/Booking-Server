2f5e33f96c6c05a86471845d07dc860ca1ee42f0
Ares Kise <areskise@gmail.com>
2022-12-18 15:35:09 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const hotelRoutes = require('./routes/hotel');
+const roomRoutes = require('./routes/room');
+const transactionRoutes = require('./routes/transaction');
+const userRoutes = require('./routes/user');
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined'));
+app.use(express.json());
+
+app.use(userRoutes);
+app.use(hotelRoutes);
+app.use(roomRoutes);
+app.use(transactionRoutes);
+
+mongoose
+    .connect("mongodb+srv://areskise:24110399@cluster0.vsce8sb.mongodb.net/booking?retryWrites=true&w=majority")
+    .then(result => {
+        app.listen(5000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
